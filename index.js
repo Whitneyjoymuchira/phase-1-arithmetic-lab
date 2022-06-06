4915424162768836055553c1d43054cb590ec259
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-07 01:02:46 +0300

    final

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=31
+const num2=2
+const multiply=num1*num2;
 
-// Write your code here
+const random = Math.floor(Math.random() *100)+ 1;
+
+const num4=29;
+const num3=5;
+const mod = num4%num3;
+
+const max = Math.max(10,14,17,20);
